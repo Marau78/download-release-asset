7157c9379954689a7c9d62cbf7a44e0bb6cddf6b
Christophe Rivier <chrivier@gmail.com>
2021-03-20 18:53:19 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,22 @@
+# action download release asset
+An action that downloads and extracts uploaded asset(artifact) associated with given release.
+
+## Usage
+
+```yaml
+- name: Download assets release
+  uses: ChristopheRivier/action-download-release-asset@v1
+  with:
+    # Optional, GitHub token mandatory for private repo
+    github_token: ${{secrets.GITHUB_TOKEN}}
+    # Mandatory, release tag name
+    release_name: v1.0.0
+    # Optional, uploaded asset name,
+    # will download all assets if not specified
+    # and extract them if end by .zip.
+    asset_name: name of the artifact if null extrac all
+    # Optional, directory where to extract artifact
+    dest_path: extract_here
+    # Optional, defaults to current repo
+    repo: ${{github.repository}}
+```
---
+++ script.sh
@@ -0,0 +1,31 @@
+
+
+url_api="https://api.github.com/repos/$repo/releases/tags/${release_name}"
+
+if [[ -z $asset_name ]]
+then
+echo "in the null"
+   artifact_names=($(curl -H "Authorization: token $github_token" "$url_api" | jq '.assets[]  | .name' | sed 's/\"//g'))
+   urls_art=($(curl -H "Authorization: token $github_token" "$url_api" | jq '.assets[]  | .url' | sed 's/\"//g'))
+ else
+   artifact_names=$asset_name
+   urls_art=$(curl -H "Authorization: token $github_token" "$url_api" | jq ".assets[] | select(.name==\"$asset_name\") | .url" | sed 's/\"//g')
+fi
+
+if [[ -z $dest_path ]]
+then
+    dest_path="./"
+fi
+
+mkdir -p $dest_path
+
+cd $dest_path
+len=${#urls_art[@]}
+for (( i=0; i< $len;i++ ));
+do
+    # download the artifacts
+    curl -vLJO -H 'Accept: application/octet-stream' -H "Authorization: token $github_token" ${urls_art[$i]}
+
+    [[ ${artifact_names[$i]} == *".zip" ]] && unzip ${artifact_names[$i]}
+
+done
